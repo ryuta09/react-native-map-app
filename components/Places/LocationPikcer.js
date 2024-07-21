3ac9cebc80e29a11426dc6007d5b3d7600f23fd1
古賀龍太 <kogaryuuta@kogaryuutanoMac-mini.local>
2024-07-21 20:17:44 +0900

    feat: locationをimportしてcameraと同様のパーミッションの設定

--- components/Places/LocationPikcer.js
+++ components/Places/LocationPikcer.js
@@ -1,9 +1,39 @@
-import { StyleSheet, View } from "react-native";
+import { Alert, StyleSheet, View } from "react-native";
+import {
+  getCurrentPositionAsync,
+  useForegroundPermissions,
+  PermissionStatus,
+} from "expo-location";
 import OutlineButton from "../../UI/OutlinedButton";
 import { Colors } from "../../constants/colors";
 
 function LocationPicker() {
-  function getLocatoinHandler() {}
+  const [locationPermissionInformation, requestPermission] =
+    useForegroundPermissions();
+  async function veritfyPermissions() {
+    if (
+      locationPermissionInformation.status === PermissionStatus.UNDETERMINED
+    ) {
+      const permissionResponse = await requestPermission();
+      return permissionResponse.granted;
+    }
+
+    // 拒否
+    if (locationPermissionInformation.status === PermissionStatus.DENIED) {
+      Alert.alert("権限が不足しています。");
+      return false;
+    }
+
+    return true;
+  }
+  async function getLocatoinHandler() {
+    const hasPermission = await veritfyPermissions();
+    if (!hasPermission) {
+      return;
+    }
+    const locatoin = await getCurrentPositionAsync();
+    console.log(locatoin);
+  }
 
   function pickOnMapHandler() {}
 
@@ -11,7 +41,7 @@ function LocationPicker() {
     <View>
       <View style={styles.mapPreview}></View>
       <View style={styles.actions}>
-        <OutlineButton icon={"location"} noPress={getLocatoinHandler}>
+        <OutlineButton icon={"location"} onPress={getLocatoinHandler}>
           Locate User
         </OutlineButton>
         <OutlineButton icon={"map"} onPress={pickOnMapHandler}>
